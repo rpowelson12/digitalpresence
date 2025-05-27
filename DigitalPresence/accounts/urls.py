b916522abeb44eb3ebcf1703635e3420dc4aca62
rpowelson12 <randy.powelson@yahoo.com>
2025-05-26 20:22:05 -0700

    login/out done

--- DigitalPresence/accounts/urls.py
+++ DigitalPresence/accounts/urls.py
@@ -1,12 +1,15 @@
-from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from . import views
+from .forms import LoginForm
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html', form_class=LoginForm), name='login'),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path('signup/', views.signup, name='signup'),
     path('pricing/', views.pricing, name='pricing'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- DigitalPresence/accounts/views.py
+++ DigitalPresence/accounts/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import SignupForm, LoginForm
 
 # Create your views here.
-def login(request):
-    return render(request,'accounts/login.html')
-
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            authenticate(username=user.first_name, password=user.password)
+
+            if user is not None:
+                login(request, user)
+
+                return redirect('/dashboard/')
+    else: 
+        form = SignupForm()
+
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
 
 def pricing(request):
     return render(request, 'accounts/pricing.html')
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        username = request.user.username
+        context = {'username': username}
+
+    else:
+        context = {'username': "Guest"}
+    return render(request, 'accounts/dashboard.html', context)
